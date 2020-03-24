dfb85b58bea36e6f1139e0e80b635d2a8fe26207
ykz45469 <ykz45469@ly.com>
2020-03-24 12:50:48 +0800

    提交部署代码

---
+++ build/precache-manifest.de3cd99f58680292272b823d0b49fce0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a49d70ef2aeb67567ab7a33bf3767e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "881cc169f74e4540435a",
+    "url": "./static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "31ba13dff1c7238827c5",
+    "url": "./static/css/main.fac8cb18.chunk.css"
+  },
+  {
+    "revision": "881cc169f74e4540435a",
+    "url": "./static/js/2.0eeecb6e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "./static/js/2.0eeecb6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31ba13dff1c7238827c5",
+    "url": "./static/js/main.0cbb6a80.chunk.js"
+  },
+  {
+    "revision": "b75f95517f7bc82cf5d3",
+    "url": "./static/js/runtime-main.9e003f1d.js"
+  },
+  {
+    "revision": "25810de46aaca1f6dafce61812131ad5",
+    "url": "./static/media/logo.25810de4.png"
+  }
+]);
