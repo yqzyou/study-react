1bf732151051fa4e195bb81f6b3f1cc02e6b1365
乐康智 <yuekangzhi@zalldigital.com>
2022-11-01 16:42:41 +0800

    feat: 提交

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Next.js 博客网站</h1>
+            <p>
+                这个博客网站将使用 <a href="https://nextjs.org/">Next.js</a>。
+            </p>
+        </>
+    );
+}
